15440b8dfd80fe4f213086d484875a863168ef75
Hery <thitphavanh.psv@gmail.com>
2022-06-07 16:00:38 +0700

    update

--- lib/src/pages/home/widgets/stock.dart
+++ lib/src/pages/home/widgets/stock.dart
@@ -2,32 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project_manage/src/pages/home/widgets/product_item.dart';
 import 'package:flutter_project_manage/src/services/network_service.dart';
 
+import '../../../models/post.dart';
+
 class Stock extends StatelessWidget {
   final _spacing = 4.0;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
+    return FutureBuilder<List<Post>?>(
       future: NetworkService().fetchPosts(0),
-      builder: (context, snapshot) => GridView.builder(
-        padding: EdgeInsets.only(
-          left: _spacing,
-          right: _spacing,
-          top: _spacing,
-          bottom: 150,
-        ),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.8,
-          crossAxisSpacing: _spacing,
-          mainAxisSpacing: _spacing,
-        ),
-        itemBuilder: (context, index) => LayoutBuilder(
-          builder: (context, BoxConstraints constraints) {
-            return ProductItem(constraints.maxHeight);
-          },
-        ),
-        itemCount: 6,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Post>? post = snapshot.data;
+          if (post == null || post.isEmpty) {
+            return Container(
+              margin: const EdgeInsets.only(top: 22.0),
+              alignment: Alignment.topCenter,
+              child: const Text('No Data'),
+            );
+          }
+          return _buildProductGridView(post);
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(snapshot.error.toString()),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  GridView _buildProductGridView(List<Post> post) {
+    return GridView.builder(
+      padding: EdgeInsets.only(
+        left: _spacing,
+        right: _spacing,
+        top: _spacing,
+        bottom: 150,
       ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.8,
+        crossAxisSpacing: _spacing,
+        mainAxisSpacing: _spacing,
+      ),
+      itemBuilder: (context, index) => LayoutBuilder(
+        builder: (context, BoxConstraints constraints) {
+          return ProductItem(constraints.maxHeight);
+        },
+      ),
+      itemCount: post.length,
     );
   }
 }
